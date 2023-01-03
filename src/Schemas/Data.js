0acee358dd2923adb2475a9eec5413dfe8674764
Ricardo Rico <programador5781@gmail.com>
2023-01-02 19:34:09 -0500

    second commit

---
+++ src/Schemas/Data.js
@@ -0,0 +1,131 @@
+export const Data = {
+  profile: {
+    name: "Ricardo Rico",
+    ocupation: "FullStack Developer",
+    location: "Santa Marta, Colombia",
+    email: "rricoz.co@gmail.com",
+    telephone: "+57 315-535-7926",
+    image: "https://avatars.githubusercontent.com/u/88601627?v=4",
+  },
+  aboutMe: {
+    label: "Perfil",
+    description:
+      "Ingeniero de Software en progreso, capacitado para desarrollar aplicaciones informáticas en diversas tecnologías y múltiples lenguajes de programación, aplicando metodologías ágiles de desarrollo, incluyendo identificación de necesidades y diseños de sistemas basados en modelos de desarrollo.",
+  },
+  skills: {
+    technicalLabel: "Tecnologías",
+    softLabel: "Skills",
+    technicalSkills: [
+      "HTML5",
+      "JavaScript",
+      "React",
+      "Redux",
+      "NodeJS",
+      "Express",
+      "Python",
+      "Java",
+      "PostgreSQL",
+      "Git",
+    ],
+    softSkills: [
+      "Capacidad de análisis",
+      "Resolución de Problemas",
+      "Pensamiento Crítico",
+      "Trabajo en equipo",
+      "Comunicación",
+      "Proactividad",
+      "Creatividad",
+      "Autodidacta",
+      "Mentoria",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "ricardo-ricoz",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/ricardo-ricoz",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "programador5781",
+        name: "github",
+        url: "https://github.com/programador5781",
+        className: "bxl-github",
+      },
+      {
+        label: "rricozDev",
+        name: "twiiter",
+        url: "https://twitter.com/rricozDev",
+        className: "bxl-twitter",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "Dic. 2022 - Dic. 2022",
+        company: "Henry",
+        description: [
+          "FRONTEND | Desarrollo de una aplicación React/ Redux, con Landing page que contiene una imagen representativa del proyecto, botón home para ingresar a la ruta principal. Entrada de búsqueda para encontrar razas de perros por nombre. Área donde se puede ver la lista de razas de perros .BACKEND | Desarrollo de un servidor en Node/Express."
+        ],
+      },
+      {
+        title: "CO-LIDER INVESTIGADOR",
+        period: "Feb. 2022 - Jul. 2022",
+        company: "Universidad de Cartagena, CTM",
+        description: [
+          "Participación en el I Seminario de Investigación y Emprendimiento del Programa Ing. De Software: Promoviendo el Desarrollo Social Sostenible de los Territorios..",
+        ],
+      },
+      {
+        title: "GERENTE DE VENTAS",
+        period: "Feb. 2014 - Mar. 2015",
+        company: "Homecenter Decor Panamá",
+        description: [
+          "Apertura de almacén, control de la fuerza de ventas, controlar indicadores, coordinar tareas,  brindar información e intercambiarla con funcionarios y jefes de otras áreas para el desempeño  de las funciones de la empresa con mayor eficiencia. Lider de proyecto 2014-2015 grupo JTM.",
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Ingeniero de Software en progreso.",
+        date: "2021 a la fecha",
+        institution: "Universidad de Cartagena",
+      },
+      {
+        career: "Responsive Web Design.",
+        date: "2022",
+        institution: "freeCodeCamp",
+      },
+      {
+        career: "Full Stack Developer.",
+        date: "Agosto 2022 - actualidad.",
+        institution: "Henry",
+      },
+      {
+        career: "DataBase Design.",
+        date: "2022",
+        institution: "Oracle Academy",
+      },
+      {
+        career: "Java Foundation.",
+        date: "2022",
+        institution: "Oracle Academy",
+      },
+    ],
+    proyects: [
+      {
+        name: "Plataforma Digital",
+        company: "Universidad de Cartagena",
+        period: "Ene. 2022 - actualidad.",
+        description: [
+          "Diseñar una plataforma interactiva para desarrollar habilidades  en el idioma inglés en los estudiantes de la Universidad de Cartagena, facultad de Ingenieria - CTM mediante actividades ONLINE y OFFLINE.",
+        ],
+      },
+      
+    ],
+  },
+};
